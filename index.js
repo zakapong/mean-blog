d19c53a48e87bfc3ad8a918654bdb30a0da120f2
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-10-04 12:14:27 +0200

    register-api

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+/* ===================
+   Import Node Modules
+=================== */
+const express = require('express'); // Fast, unopinionated, minimalist web framework for node.
+const app = express(); // Initiate Express Application
+const router = express.Router(); // Creates a new router object.
+const mongoose = require('mongoose'); // Node Tool for MongoDB
 const config = require('./config/database'); // Mongoose Config
 const path = require('path'); // NodeJS Package for file paths
-
+const authentication = require('./routes/authentication')(router); // Import Authentication Routes
+const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 
 // Database Connection
-// mongoose.Promise = global.Promise;
-mongoose.connect(config.uri, { useMongoClient: true, promiseLibrary: global.Promise }, (err) => {
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
   if (err) {
     console.log('Could NOT connect to database: ', err);
   } else {
-      console.log(config.secret);
     console.log('Connected to database: ' + config.db);
   }
 });
 
-
-// Provide static directory for frontend
-app.use(express.static(__dirname + '/client/dist/'));
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
+app.use('/authentication', authentication);
 
 // Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
-
-app.listen(8080,()=>{
-    
-    console.log('Liestening on port 8080');
+// Start Server: Listen on port 8080
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
 });
