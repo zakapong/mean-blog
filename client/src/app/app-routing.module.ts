048130937cd80e8221576d980a3adb44d91ccf1e
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-10-09 15:02:02 +0200

    adding jwt authorization

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {RegisterComponent} from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 // Our Array of Angular 2 Routes
 const appRoutes: Routes = [
@@ -17,7 +19,15 @@ const appRoutes: Routes = [
   },
   {
   path: 'register',
-  component: RegisterComponent  
+  component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent // Login Route
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent // Profile Route
   },
   { path: '**', component: HomeComponent } // The "Catch-All" Route
 ];
