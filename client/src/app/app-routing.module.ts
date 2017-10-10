d284407169181b3438dcd4f5464a2b509fe8ffa3
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-10-10 09:36:06 +0200

    auth gurad and routing fix

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,35 +1,40 @@
-
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import {RegisterComponent} from './components/register/register.component';
+import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { AuthGuard } from './guards/auth.guard';
+import { NotAuthGuard } from './guards/notAuth.guard';
 
 // Our Array of Angular 2 Routes
 const appRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent // The Default Route
+    component: HomeComponent // Default Route
   },
   {
     path: 'dashboard',
-    component: DashboardComponent // The Dashboard Route
+    component: DashboardComponent, // Dashboard Route,
+    canActivate: [AuthGuard] // User must be logged in to view this route
   },
   {
-  path: 'register',
-  component: RegisterComponent
+    path: 'register',
+    component: RegisterComponent, // Register Route
+    canActivate: [NotAuthGuard] // User must NOT be logged in to view this route
   },
   {
     path: 'login',
-    component: LoginComponent // Login Route
+    component: LoginComponent, // Login Route
+    canActivate: [NotAuthGuard] // User must NOT be logged in to view this route
   },
   {
     path: 'profile',
-    component: ProfileComponent // Profile Route
+    component: ProfileComponent, // Profile Route
+    canActivate: [AuthGuard] // User must be logged in to view this route
   },
-  { path: '**', component: HomeComponent } // The "Catch-All" Route
+  { path: '**', component: HomeComponent } // "Catch-All" Route
 ];
 
 @NgModule({
