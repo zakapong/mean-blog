aba843b47aa8f324b3d185213808c13ab3c99803
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-10-11 15:33:23 +0200

    creating blog

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { AuthGuard } from './guards/auth.guard';
 import { NotAuthGuard } from './guards/notAuth.guard';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { NotAuthGuard } from './guards/notAuth.guard';
     DashboardComponent,
     RegisterComponent,
     LoginComponent,
-    ProfileComponent
+    ProfileComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const express = require('express'); // Fast, unopinionated, minimalist web frame
 const app = express(); // Initiate Express Application
 const router = express.Router(); // Creates a new router object.
 const mongoose = require('mongoose'); // Node Tool for MongoDB
+mongoose.Promise = global.Promise;
 const config = require('./config/database'); // Mongoose Config
 const path = require('path'); // NodeJS Package for file paths
 const authentication = require('./routes/authentication')(router); // Import Authentication Routes
+const blogs = require('./routes/blogs')(router); // Import Blog Routes
 const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 const cors = require('cors'); // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
 
 // Database Connection
-mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
   if (err) {
     console.log('Could NOT connect to database: ', err);
@@ -26,7 +27,8 @@ app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
-app.use('/authentication', authentication);
+app.use('/authentication', authentication); // Use Authentication routes in application
+app.use('/blogs', blogs); // Use Blog routes in application
 
 // Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
