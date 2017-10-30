69dafbc0c980991a74f11e46d4c260ad0a734b83
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-10-30 09:01:55 +0100

    deployment fixes

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides c
 
 // Export config object
 module.exports = {
-  uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
+//  uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
+  uri: 'mongodb://zakapong:Qazwsx!24@ds139585.mlab.com:39585/mean-blog',   // production
   secret: crypto, // Cryto-created secret
-  db: 'mean-angular-2' // Database name
+  db: 'mean-blog' // Database name
 }
